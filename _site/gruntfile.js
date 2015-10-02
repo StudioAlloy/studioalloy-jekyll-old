0fe9a2cd75b6dd3059cf5ff16ad6f110c37c61a2
Mitchel van Eijgen <mvaneijgen@gmail.com>
2015-10-02 10:35:13 +0200

    first steps in makeing landing pages responsive

--- _site/gruntfile.js
+++ _site/gruntfile.js
@@ -1,30 +1,35 @@
 /* globals module, require */
 
 module.exports = function(grunt) {
-
+  
   "use strict";
-
+  
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
-
+    
     sass: {
       global: {
         options: {
           style: "expanded"
         },
         files: {
-          "css/main-unprefixed.css": "_sass/main.scss"
+          "css/main-unprefixed.css": "_sass/main.scss",
+          "css/landing-unprefixed.css": "_sass/landing.scss"
         }
       }
     },
-
+    
     autoprefixer: {
       global: {
         src: "css/main-unprefixed.css",
         dest: "css/main.css"
+      },
+      mine: {
+        src: "css/landing-unprefixed.css",
+        dest: "css/landing.css"
       }
     },
-
+    
     shell: {
       jekyllServe: {
         command: "jekyll serve --config _config-dev.yml"
@@ -33,7 +38,7 @@ module.exports = function(grunt) {
         command: "jekyll build --config _config-dev.yml"
       }
     },
-
+    
     watch: {
       options: {
         livereload: true
@@ -50,14 +55,14 @@ module.exports = function(grunt) {
         files: ["_sass/*.scss"],
         tasks: ["sass", "autoprefixer", "shell:jekyllBuild"]
       }
-
+      
     }
-
+    
   });
-
+  
   require("load-grunt-tasks")(grunt);
-
+  
   grunt.registerTask("serve", ["shell:jekyllServe"]);
   grunt.registerTask("default", ["sass", "autoprefixer", "shell:jekyllBuild", "watch"]);
-
+  
 };
